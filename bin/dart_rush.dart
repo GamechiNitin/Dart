38852d28fcd2d3172bb4bbaca85715642b52c915
Nitin Gamechi <nitingamechi1696@gmail.com>
2022-10-31 16:40:21 +0530

    First Commit

---
+++ bin/dart_rush.dart
@@ -0,0 +1,5 @@
+import 'package:dart_rush/dart_rush.dart' as dart_rush;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_rush.calculate()}!');
+}
---
+++ test/dart_rush_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_rush/dart_rush.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
