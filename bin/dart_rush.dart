f67d2c461c0ad7e09d2af7aba94dd8258280118b
Nitin Gamechi <nitingamechi1696@gmail.com>
2022-11-05 23:54:02 +0530

    Average value from terminal

--- bin/dart_rush.dart
+++ bin/dart_rush.dart
@@ -2,4 +2,6 @@ import 'package:dart_rush/dart_rush.dart' as dart_rush;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_rush.calculate()}!');
+  print(
+      "AVERAGE FROM TERMINAL :  ${arguments.fold<int>(0, (previousValue, element) => previousValue + int.parse(element))}");
 }
